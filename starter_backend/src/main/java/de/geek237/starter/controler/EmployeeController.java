738c31a301da1712c0779e373df75a37e7006677
Donald Donchi Fofack <donald.donchi.fofack@mni.thm.de>
2022-01-07 07:12:18 +0100

    Spring Controller with REST API - /api/employees

---
+++ starter_backend/src/main/java/de/geek237/starter/controler/EmployeeController.java
@@ -0,0 +1,22 @@
+package de.geek237.starter.controler;
+
+import de.geek237.starter.entity.Employee;
+import de.geek237.starter.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("http://localhost:3000/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> fetchEmployees(){
+        return employeeRepository.findAll();
+    }
+}
